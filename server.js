3c463348c3c89e692a52c5a24319f1c76b3fd7a7
jessumer06 <30969789+jessumer06@users.noreply.github.com>
2017-08-13 11:46:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get('/ui/style.css', function (req, res) {
 });
 
 app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'MG_20170507_163210_edit.jpg'));
+  res.sendFile(path.join(__dirname, 'ui', 'IMG_20170507_163210_edit.jpg'));
 });
 
 
